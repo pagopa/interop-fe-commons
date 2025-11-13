5a9298073393099715351e35b0622803b59d038d
borgesis95 <salvatore.borgesi@pagopa.it>
2025-11-13 12:34:58 +0100

    build: bump MUI and react-testing-library

--- src/features/pagination/__tests__/Pagination.test.tsx
+++ src/features/pagination/__tests__/Pagination.test.tsx
@@ -1,20 +1,50 @@
 import React from 'react'
-import { Pagination } from '../components/Pagination'
-import { vi } from 'vitest'
-import renderer from 'react-test-renderer'
+import { render, waitFor } from '@testing-library/react'
+import { PaginationExample } from '../stories/PaginationExample'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
 
-describe('Pagination component', () => {
-  it('Renders correctly', () => {
-    const tree = renderer
-      .create(<Pagination pageNum={1} totalPages={2} onPageChange={vi.fn()} />)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+describe.only('Pagination component', () => {
+  // const renderPagination = () => {
+  //   return render(<Pagination pageNum={1} totalPages={2} onPageChange={vi.fn()} />)
+  // }
 
-  it('Does not render when total pages are equal 1', () => {
-    const tree = renderer
-      .create(<Pagination pageNum={1} totalPages={1} onPageChange={vi.fn()} />)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
+  describe('Render pagination without rowsPerPage selector and with 100 as total elements', () => {
+    it('Should render correctly', () => {
+      const screen = render(<PaginationExample />)
+      expect(screen).toBeDefined()
+    })
+
+    it('Should not render select with rows per page options', () => {
+      const screen = render(<PaginationExample />)
+      screen.debug()
+      const selectElement = screen.queryByTestId('rows-per-page-select')
+      expect(selectElement).toBeNull()
+    })
+
+    it('If total elements are 100 and default limit is 10, total pages should be 10', () => {
+      const screen = render(<PaginationExample />)
+      const paginationElement = screen.getByRole('navigation')
+
+      expect(paginationElement).toBeDefined()
+      const lastButton = screen.getByText('10')
+      expect(lastButton).toBeDefined()
+      const nextButton = screen.queryByText('11')
+      expect(nextButton).toBeNull()
+    })
+
+    it('If user click on page 5, page 5 should be selected', async () => {
+      const screen = render(<PaginationExample />)
+      const pageButton = screen.getByRole('button', {
+        name: 'Go to page 5',
+      })
+
+      expect(pageButton).not.toBeNull()
+      await userEvent.click(pageButton, { delay: 2 })
+      await waitFor(() => {
+        const buttonAfterClick = screen.getByText('5')
+        expect(buttonAfterClick).toHaveClass('Mui-selected')
+      })
+    })
   })
 })
