0473c5a0dcbea84f140e3c0486438e87899c16df
borgesis95 <salvatore.borgesi@pagopa.it>
2026-09-22 17:46:49 +0200

    test: fix Table snapshot and pagination test

--- src/features/pagination/__tests__/Pagination.test.tsx
+++ src/features/pagination/__tests__/Pagination.test.tsx
@@ -1,45 +1,24 @@
 import React from 'react'
 import { render, waitFor, within } from '@testing-library/react'
-import { Pagination } from '../components/Pagination'
+import { PaginationExample } from '../stories/PaginationExample'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
-
-const PaginationHarness: React.FC<{ withRowsPerPage?: boolean }> = ({ withRowsPerPage }) => {
-  const [pageNum, setPageNum] = React.useState(1)
-  const [limit, setLimit] = React.useState(10)
-
-  return (
-    <Pagination
-      totalPages={10}
-      pageNum={pageNum}
-      onPageChange={setPageNum}
-      {...(withRowsPerPage
-        ? {
-            rowPerPageOptions: {
-              limit,
-              onLimitChange: setLimit,
-            },
-          }
-        : {})}
-    />
-  )
-}
 
 describe('Pagination component', () => {
   describe('Render pagination without rowsPerPage selector and with 100 as total elements', () => {
     it('Should render correctly', () => {
-      const screen = render(<PaginationHarness />)
+      const screen = render(<PaginationExample />)
       expect(screen).toBeDefined()
     })
 
     it('Should not render select with rows per page options', () => {
-      const screen = render(<PaginationHarness />)
+      const screen = render(<PaginationExample />)
       const selectElement = screen.queryByTestId('rows-per-page-select')
       expect(selectElement).toBeNull()
     })
 
     it('If total elements are 100 and default limit is 10, total pages should be 10', () => {
-      const screen = render(<PaginationHarness />)
+      const screen = render(<PaginationExample />)
       const paginationElement = screen.getByRole('navigation')
 
       expect(paginationElement).toBeDefined()
@@ -50,30 +29,28 @@ describe('Pagination component', () => {
     })
 
     it('If user click on page 5, page 5 should be selected', async () => {
-      const screen = render(<PaginationHarness />)
-      const pageButton = screen.getByRole('button', {
-        name: 'Go to page 5',
-      })
+      const screen = render(<PaginationExample />)
+      const pageButton = screen.getByRole('button', { name: 'Go to page 5' })
 
       expect(pageButton).not.toBeNull()
       await userEvent.click(pageButton, { delay: 2 })
       await waitFor(() => {
-        const buttonAfterClick = screen.getByRole('button', { name: 'page 5' })
-        expect(buttonAfterClick).toHaveAttribute('aria-current', 'true')
+        const buttonAfterClick = screen.getByText('5')
+        expect(buttonAfterClick).toHaveClass('Mui-selected')
       })
     })
   })
 
   describe('Render pagination with rowsPerPage selector enabled and with 100 as total elements', () => {
     it('Should render correctly', () => {
-      const screen = render(<PaginationHarness withRowsPerPage />)
+      const screen = render(<PaginationExample withRowsPerPage />)
       expect(screen).toBeDefined()
     })
 
     it('Should be available [10,24,36] as rows per page as default options', async () => {
-      const screen = render(<PaginationHarness withRowsPerPage />)
+      const screen = render(<PaginationExample withRowsPerPage />)
       const selectElement = screen.getByTestId('rows-per-page-select')
-      const selectButton = within(selectElement).getByRole('combobox')
+      const selectButton = within(selectElement).getByRole('button')
 
       expect(selectElement).toBeDefined()
       await userEvent.click(selectButton)
